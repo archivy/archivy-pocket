b691f8c83efb3a0d253ee894751e1f787e20dc51
Uzay-G <uzgirit@gmail.com>
2020-12-05 23:55:12 +0100

    v0.1.6 add reqs.txt

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+with open('requirements.txt', encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+    install_requires = [x.strip() for x in all_reqs]
+
 setup(
     name='archivy_pocket',
-    version='0.1.5',
+    version='0.1.6',
     author="Uzay-G",
     author_email="halcyon@disroot.org",
     description=(
@@ -18,6 +23,7 @@ setup(
         "License :: OSI Approved :: MIT License"
     ],
     packages=find_packages(),
+    install_requires=install_requires
     entry_points='''
         [archivy.plugins]
         pocket=archivy_pocket:pocket
